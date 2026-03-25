dceb85aa3f0b82f9895915e1b0f1014d8e7f561a
Hans-255 <happychedegotz@gmail.com>
2026-03-26 02:00:08 +0300

    Update: HansTz/general.js

---
+++ HansTz/general.js
@@ -0,0 +1,78 @@
+
+const { adams } = require('../Hans/adams");
+const conf = require("../config");
+
+adams({ nomCom: "owner", categorie: "General", reaction: "🚘" }, async (dest, zk, commandeOptions) => {
+    const { ms, mybotpic } = commandeOptions;
+    
+    const vcard =
+        'BEGIN:VCARD\n' +
+        'VERSION:3.0\n' +
+        'FN:' + conf.OWNER_NAME + '\n' +
+        'ORG:BWM-XMD;\n' +
+        'TEL;type=CELL;type=VOICE;waid=' + conf.NUMERO_OWNER + ':+' + conf.NUMERO_OWNER + '\n' +
+        'END:VCARD';
+    
+    zk.sendMessage(dest, {
+        contacts: {
+            displayName: conf.OWNER_NAME,
+            contacts: [{ vcard }],
+        },
+    }, { quoted: ms });
+});
+
+adams({ nomCom: "dev", categorie: "General", reaction: "🚘" }, async (dest, zk, commandeOptions) => {
+    const { ms, mybotpic } = commandeOptions;
+
+    const devs = [
+      { nom: "Hans", number: "255753668403" }
+    ];
+
+    let message = "WELCOME TO VORTEX md HELP CENTER! CONTACT THE DEVELOPER:\n\n";
+    for (const dev of devs) {
+      message += `• ${dev.nom} : https://wa.me/${dev.number}\n`;
+    }
+    
+    var lien = mybotpic();
+    if (lien.match(/\.(mp4|gif)$/i)) {
+        try {
+            zk.sendMessage(dest, { video: { url: lien }, caption: message }, { quoted: ms });
+        }
+        catch (e) {
+            console.log("Error sending message: " + e);
+            repondre("Error sending message: " + e);
+        }
+    } 
+    else if (lien.match(/\.(jpeg|png|jpg)$/i)) {
+        try {
+            zk.sendMessage(dest, { image: { url: lien }, caption: message }, { quoted: ms });
+        }
+        catch (e) {
+            console.log("Error sending message: " + e);
+            repondre("Error sending message: " + e);
+        }
+    } 
+    else {
+        repondre("Error: Invalid media link");
+    }
+});
+
+adams({ nomCom: "support", categorie: "General" }, async (dest, zk, commandeOptions) => {
+    const { ms, repondre, auteurMessage } = commandeOptions; 
+    
+    const supportMessage = `
+THANK YOU FOR CHOOSING BWM-XMD
+
+SUPPORT LINKS:
+☉ Channel: https://whatsapp.com/channel/0029Vb7JRfvCRs1gTmsCB812
+☉ Group: https://chat.whatsapp.com/F5BXJci8EDS9AJ6sfKMXIS
+☉ YouTube: https://www.youtube.com/@ibrahimaitech
+
+Created by HansTz
+`;
+    
+    repondre(supportMessage);
+    await zk.sendMessage(auteurMessage, {
+        text: `THANK YOU FOR CHOOSING BWM-XMD, MAKE SURE YOU FOLLOW THESE LINKS.`
+    }, { quoted: ms });
+});
