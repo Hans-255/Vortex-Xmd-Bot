f4cadafee1cab4ce285b7744a86d7516eae221c3
Hans-255 <happychedegotz@gmail.com>
2026-03-26 14:07:06 +0300

    fix: showAdAttribution false + serverMessageId -1 in menu2.js (invisible msg fix)

--- HansTz/menu2.js
+++ HansTz/menu2.js
@@ -110,7 +110,7 @@ adams({ nomCom: "menu", categorie: "General" }, async (dest, zk, commandeOptions
     forwardedNewsletterMessageInfo: {
         newsletterJid: '120363421513037430@newsletter',
         newsletterName: `${contactName}`, // Uses sender's name
-        serverMessageId: Math.floor(100000 + Math.random() * 900000),
+        serverMessageId: -1 * 900000),
     },
 };
 
